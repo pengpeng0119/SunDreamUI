d8ecce14f0056143374016706c54dfccc14b958c
彭文豪 <2279265610@qq.com>
2023-05-14 16:58:40 +0800

    完成全部初始配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { memo } from 'react'
 
-function App() {
+const App = memo(() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <a href="www.baidu.com">111111</a>
+      <h1>hello world</h1>
+      <h2>hello world</h2>
+      <h3>hello world</h3>
+      <h4>hello world</h4>
+      <h5>hello world</h5>
+      <code>
+        const a = 'b'
+      </code>
     </div>
-  );
-}
+  )
+})
 
-export default App;
+export default App
