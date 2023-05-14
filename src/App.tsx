6b155a09eafa2dc7ce5996a07413adf8d0da4420
彭文豪 <2279265610@qq.com>
2023-05-15 00:16:40 +0800

    完成大体button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { memo } from 'react'
+import Button, { ButtonType } from './components/Button/button'
 
 const App = memo(() => {
   return (
@@ -7,6 +8,9 @@ const App = memo(() => {
       <h1>hello world</h1>
       <h2>hello world</h2>
       <h4>hello world</h4>  
+      <Button disabled>hello</Button>
+      <Button btnType={ButtonType.Primary}>hello</Button>
+      <Button btnType={ButtonType.Link} href='http://www.baidu.com' disabled>BaiDu</Button>
       <code>
         const a = 'b'
       </code>
