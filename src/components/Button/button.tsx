a04ad5c36147047b2027883cade72c2a71cc1458
彭文豪 <2279265610@qq.com>
2023-05-19 23:36:16 +0800

    备份2

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,33 +1,35 @@
 import React, { memo } from 'react'
-import classNames from 'classnames';
-export enum ButtonSize{
-  large='lg',
-  Small='sm'
+import classNames from 'classnames'
+export enum ButtonSize {
+  large = 'lg',
+  Small = 'sm'
 }
 
-export enum ButtonType{
-  Primary='primary',
-  Default='default',
-  Danger='danger',
-  Link='link',
-  Success='success'
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link',
+  Success = 'success'
 }
 
-interface BaseButtonProps{
-  className?:string;
-  disabled?:boolean;
-  ellipse?:boolean;
-  rounded?:boolean;
-  size?:ButtonSize;
-  btnType?:ButtonType;
-  children:React.ReactNode;
-  href?:string
+interface BaseButtonProps {
+  className?: string
+  disabled?: boolean
+  ellipse?: boolean
+  rounded?: boolean
+  size?: ButtonSize
+  btnType?: ButtonType
+  children: React.ReactNode
+  href?: string
 }
 
-type NativeButtonProps=BaseButtonProps&React.ButtonHTMLAttributes<HTMLElement>
-type AnchorButtonProps=BaseButtonProps&React.AnchorHTMLAttributes<HTMLElement>
-export type ButtonProps =Partial<NativeButtonProps & AnchorButtonProps>
-const Button:React.FC<ButtonProps>=(props)=>{
+type NativeButtonProps = BaseButtonProps &
+  React.ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps &
+  React.AnchorHTMLAttributes<HTMLElement>
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+const Button: React.FC<ButtonProps> = props => {
   const {
     btnType,
     className,
@@ -36,42 +38,33 @@ const Button:React.FC<ButtonProps>=(props)=>{
     size,
     children,
     href,
-    ellipse ,
+    ellipse,
     ...restProps
-  }=props
-  const classes=classNames('btn',className,{
-    [`btn-${btnType}`]:btnType,
-    [`btn-${size}`]:size,
-    'disabled':(btnType===ButtonType.Link)&&disabled,
-    'btn-ellipse':ellipse,
-    'btn-rounded':rounded
+  } = props
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    disabled: btnType === ButtonType.Link && disabled,
+    'btn-ellipse': ellipse,
+    'btn-rounded': rounded
   })
-  if(btnType===ButtonType.Link&&href){
+  if (btnType === ButtonType.Link && href) {
     return (
-      <a
-      className={classes}
-      href={href}
-      {...restProps}
-      >{children}
+      <a className={classes} href={href} {...restProps}>
+        {children}
       </a>
     )
-  }else{
-    return(
-    <button
-    className={classes}
-    disabled={disabled}
-    {...restProps}
-    >
-    {children}
-    </button>
+  } else {
+    return (
+      <button className={classes} disabled={disabled} {...restProps}>
+        {children}
+      </button>
     )
   }
 }
-Button.defaultProps={
-  disabled:false,
-  btnType:ButtonType.Default
+Button.defaultProps = {
+  disabled: false,
+  btnType: ButtonType.Default
 }
 
 export default Button
-
-
